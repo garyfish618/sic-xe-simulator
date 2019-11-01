033d379316206ae75fae3a2dda11f02ee9b5dc82
garyfish618 <fishellgary@gmail.com>
2019-10-31 19:48:00 -0700

    Started interpreter process

---
+++ interpreter.py
@@ -0,0 +1,11 @@
+import parser, memory
+
+class Interpreter:
+
+    def __init__(self, instruction_array):
+        self.instructions = instruction_array
+
+    def execute_next_instruction(self):
+        next_line = self.instruction
+
+
