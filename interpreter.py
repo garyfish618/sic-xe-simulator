7254f51bd7d4b6fead248d5416f386d47ea39318
Lauren DeLeon <lauren@Laurens-MacBook-Air-2.local>
2019-11-06 10:05:37 -0800

    working on the add instruction

--- interpreter.py
+++ interpreter.py
@@ -1,28 +1,35 @@
 import parser, memory
 
-
 class Interpreter:
 
     def __init__(self, instruction_array, isSimple):
         self.instructions = instruction_array
         self.is_simple = isSimple
         self.instruction_pointer = 0
-        self.memory_set = memory()
+        # self.memory_set = memory()
 
-        self.assign_addresses(instruction_array)
-
-    def assign_address(self, instruction_array):
+    def assign_address(self):
         next_address = "0000"
+        directives = ["RESW", "RESB", "BYTE", "WORD"]
 
-        for instruction in instruction_array:
+        for instruction in self.instructions:
             if instruction.name == "START":
                 next_address = instruction.args[0]
-                instruction.address = next_address
-            
-            if is_simple:
-                #Convert to hex -> Strip 0x off front -> Capitalize all characters -> Fill with 0's so string is 4 characters
-                next_address = hex(int(next_address, 16) + 3).strip("0x").upper().zfill(4) 
-                print(next_address)
+                continue
+
+            #If directive - Leave directive assignment to directives module
+            if instruction.name in directives:
+                #TODO - Execute Directive Assignment module
+                pass
+            instruction.address = next_address
+
+            print("DEBUG: " + "Instruction: " + instruction.name + " Address: " + instruction.address)
+
+            if self.is_simple:
+                # Convert to hex -> Strip 0x off front -> Capitalize all characters -> Fill with 0's so string is 4
+                # characters
+
+                next_address = hex(int(next_address, 16) + 3).strip("0x").upper().zfill(4)
 
     def execute_next_instruction(self):
         next_line = self.instructions[self.instruction_pointer]
@@ -31,16 +38,16 @@ class Interpreter:
         label = next_line.label
         arguments = next_line.args
 
-        instruction_token = determine_instruction(instruction_name)
+        instruction_token = self.determine_instruction(instruction_name)
 
-        if instruction_token == -1: 
-            raise Exception("Invalid instruction name on line") #TODO provide line number once implemented in parser
+        if instruction_token == -1:
+            raise Exception("Invalid instruction name on line")  # TODO provide line number once implemented in parser
 
         token_utilizer(instruction_name, arguments, label)
 
     def determine_instruction(self, instruction_name):
         instruction_set = {}
-         
+
         if (self.is_simple):
             instruction_set = {
                 1: "ADD",
@@ -74,44 +81,50 @@ class Interpreter:
         else:
             instruction_set = {}
 
-        return instruction_set.get(instruction_name, -1) #Returns -1 if instruction was not found
+        return instruction_set.get(instruction_name, -1)  # Returns -1 if instruction was not found
+
+    def token_utilizer(self, instruction_token, arguments, label):
+
+        if (self.isSimple):
+
+            # if instruction_token == 10:
+            #     value = __getinstruction__(arguments[0])
+                
+            #     if value == "WORD":
+                
+            #     if value == "BYTE":
+
+                
+            #     if value == "RESB":
+
+            #     if value == "RESW":
     
-    #def token_utilizer(self, instruction_token, arguments, label):
+            if instruction_token == 1: #ADD
+                print("Do ADD")
+                if arguments[1] == 'X':
+                    self.__getinstruction__(label)
 
-        # if (self.isSimple):
-        #     if instruction_token == 1: #ADD
-        #         print("Do ADD")
-            
-        #     elif instruction_token == 2: #AND
-                
-        #     elif instruction_token == 3: #COMP
-        #         #Do a SUB between m1 and m2
-        #         #Store that value in register
-        #         result = 
-
-        #     elif instruction_token == 4: #DIV
-
-        #     elif instruction_token == 5: #J
-
-                
-
-        #     elif instruction_token == 1: #ADD
-
-        #     elif instruction_token == 1: #ADD
-
-        #     elif instruction_token == 1: #ADD
-
-        #     elif instruction_token == 1: #ADD
-            
-        #     elif instruction_token == 1: #ADD
-            
-        #     elif instruction_token == 1: #ADD
+                else:
+                    intruction_label = __getinstruction__(label)
+                    instruction_mem = get_memory(intruction_label.address)
+                    A = get_registers()
+                    register_add = add_hex(instruction_mem, A)
+                    setRegisters(A, register_add)
 
 
-#Helper methods
+    def __getinstruction__(self, label):
+        for instr in self.instructions:
+            if instr.label == label:
+                return instr
+
+
+
+
+
+#------Helper Methods------#
 
 def hex2int(val, bits):
-    if (val & (1 << (bits - 1))) != 0: 
+    if (val & (1 << (bits - 1))) != 0:
         val = val - (1 << bits)
     return val
 
@@ -119,12 +132,13 @@ def int2hex(number, bits):
     if number < 0:
         return hex((1 << bits) + number)[2:]
     else:
-        return hex(number)[2:]             
+        return hex(number)[2:]
+
 
 def add_hex(x, y):
     return int2hex(hex2int(x, len(x) * 4) + hex2int(y, len(y) * 4))
 
+
 def sub_hex(x, y):
     return int2hex(hex2int(x, len(x) * 4) - hex2int(y, len(y) * 4))
-
 
---
+++ memory.py
@@ -0,0 +1,73 @@
+class Memory: 
+    # isSimple = True    #False = SIC/XE
+    def __init__(self, isSimple):
+        if isSimple:
+            self.memory = [None] * 4000      #size in bytes
+        else:
+            self.memory = [None] * 128000    #size in bytes
+
+    def get_memory(self):
+        return self.memory
+
+    def set_memory(self, hex, byte):
+        dec = int (hex, 16)
+        
+        if isSimple:
+            if dec < 0 or dec > 4000:
+                return False
+        else:
+            if dec < 0 or dec > 128000:
+                return False
+        
+        byte = get_memory(dec)
+        return  True
+
+    def view_memory(self):
+        output = []
+        for byte in self.memory:
+            output.append(byte)
+            if len(output) % 16 == 0: #print memory in groups of 8 bytes
+                print(output)
+                output = []
+        if len(output) > 0:
+            print(output)
+
+class Registery:
+    def __init__(self, isSimple):
+        if isSimple:
+            #5 registers 24 bits in length 
+            self.registers = {  
+                'A' : 0,    #Accumulator
+                'X' : 0,    #Index register
+                'L' : 0,    #Linkage register (JSUB)
+                'PC': 0,    #Program counter
+                'SW': 0     #Status word (Condition Code)
+            }
+        else:
+            #3 additional registers, 24 bit length + 1 register 48 bit length
+            self.registers = {  
+                'A' : 0,    #Accumulator
+                'X' : 0,    #Index register
+                'L' : 0,    #Linkage register (JSUB)
+                'PC': 0,    #Program counter
+                'SW': 0,    #Status word (Condition Code)
+                'B' : 0,    #Base register; used for addressing
+                'S' : 0,    #General working register
+                'T' : 0,    #General working register
+                'F' : 0     #Floating-point accumulator (48 bits)
+            }
+
+    def get_registers(self):
+        return self.registers
+
+    def set_register(self, reg, val):
+        if reg in self.registers.keys():
+            self.registers[reg] = val
+            return True
+        return False
+
+    def view_registers(self):
+        for register in self.registers:
+            print(register)
+# x = Registery(True)
+# x.viewRegisters()
