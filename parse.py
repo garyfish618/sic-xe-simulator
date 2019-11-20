db380f59da46dd7dc7cb99eb1b84d905e743b75f
Gary Fishell <garyfishell@Garys-MacBook-Air.local>
2019-11-20 15:19:20 -0800

    Added error checking, credits and line numbers

--- parse.py
+++ parse.py
@@ -3,11 +3,12 @@ import re
 
 class Instruction:
 
-    def __init__(self, label, name, args, address):
+    def __init__(self, label, name, args, address, line_num):
         self.label = label
         self.name = name
         self.args = args 
         self.address = address #Interpreter will assign addresses later
+        self.line_num = line_num
 
     def setLabel(self, label):
         return self.label
@@ -21,10 +22,12 @@ class Instruction:
 
 def read_file(file):
     instruction_list = []
+    line_num = 0
     try:
         with open(file) as f:
             content = f.readlines()
             for line in content:
+                line_num += 1
                 if line.isspace():
                     continue
                 input_list = re.split(',|\s+',line) 
@@ -32,7 +35,7 @@ def read_file(file):
                 args_array = []
                 for i in range(2, len(input_list)): 
                     args_array.append(input_list[i])
-                instruction_obj = Instruction(input_list[0], input_list[1], args_array, None) #instantiate an instruction object
+                instruction_obj = Instruction(input_list[0], input_list[1], args_array, None, line_num) #instantiate an instruction object
                 instruction_list.append(instruction_obj)
         return instruction_list
     except IOError:
