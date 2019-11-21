c098ab354bd73c377c0fa533a014cce5ed54968c
Salim Aweys <s.aweys@gmail.com>
2019-11-20 19:11:05 -0800

    Add files via upload

---
+++ tests/ArithmaticTests/Arithmatic.asm
@@ -0,0 +1,32 @@
+        START   0500
+        LDA     NUM2
+        ADD     NUM1
+        ADD     NUM2
+        ADD     NUM3
+        ADD     NUM4
+        ADD     NUM5
+
+        ADD     NUM1
+        ADD     NUM2
+        ADD     NUM3
+        ADD     NUM4
+        ADD     NUM5
+
+        ADD     NUM1
+        ADD     NUM2
+        ADD     NUM3
+        ADD     NUM4
+        ADD     NUM5
+
+        ADD     NUM1
+        ADD     NUM2
+        ADD     NUM3
+        ADD     NUM4
+        ADD     NUM5
+
+
+NUM1    WORD    -10
+NUM2    WORD    0
+NUM3    BYTE    X'0F'
+NUM4    BYTE    X'10'
+NUM5    WORD    0
