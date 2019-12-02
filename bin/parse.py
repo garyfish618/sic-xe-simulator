8b2ae928f02dc878ba5553d984f9aaab23b18094
Lauren DeLeon <lauren@Laurens-MacBook-Air-2.local>
2019-12-02 14:14:07 -0800

    working on fixing parser

--- bin/parse.py
+++ bin/parse.py
@@ -23,6 +23,7 @@ class Instruction:
 def read_file(file):
     instruction_list = []
     line_num = 0
+    input_list = ''
     try:
         with open(file) as f:
             content = f.readlines()
@@ -30,11 +31,13 @@ def read_file(file):
                 line_num += 1
                 if line.isspace() or line[0] == '.':
                     continue
-                input_list = re.split(',|\s+',line) #TODO Needs fix
+                input_list = re.split(",|\s+",line) #TODO Needs fix
+                #|C'.+?'
                 #Need to always put arguments in array regardless of one or two.
                 args_array = []
                 for i in range(2, len(input_list)): 
                     args_array.append(input_list[i])
+                    #print(args_array)
                 instruction_obj = Instruction(input_list[0], input_list[1], args_array, None, line_num) #instantiate an instruction object
                 instruction_list.append(instruction_obj)
         return instruction_list
