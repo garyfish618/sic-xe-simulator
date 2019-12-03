7448a09f0889843cfc29a9d68afc959d9fd8a38f
zalumm <s.aweys@gmail.com>
2019-12-02 20:51:31 -0800

    fixed Store functions using 2  params in hex2int

--- bin/sim_interpreter.py
+++ bin/sim_interpreter.py
@@ -388,7 +388,7 @@ class Interpreter:
             address = start_address
             for byte in bytesplit(value):
                 self.memory_set.set_memory(address, byte)
-                address = int2hex(hex2int(address,16) + 1).zfill(4)
+                address = int2hex(hex2int(address) + 1).zfill(4)
 
         elif instruction_token == 19: #STCH
             #M[RMB] = A[RMB]
